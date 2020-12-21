f1689dae057c0907519085c55264db6d6e157f43
Marcel Barbosa <marcel.sby.br@gmail.com>
2020-12-21 19:25:48 -0300

    Exercício 25 finalizado

--- 1-lista-exercicios/exercicio25.c
+++ 1-lista-exercicios/exercicio25.c
@@ -28,6 +28,34 @@ int somaColuna (int matriz[][COLS], int coluna){
   return total;
 }
 
+int somaDiagonalPrincipal (int matriz[][COLS]) {
+  int total = 0;
+for (int i = 0; i < ROWS; i++)
+      total += matriz[i][i]; 
+
+  return total;
+}
+
+int somaDiagonalSecundaria(int matriz[][COLS]) {
+  int total = 0;
+  
+  for (int r = 0; r < ROWS; r++) {
+      total += matriz[r][ROWS - 1 - r]; 
+  }
+  
+  return total;
+}
+int somaMatrizTotal (int matriz[][COLS]) {
+  int total = 0;
+
+  for (int r = 0; r < ROWS; r++) {
+    for (int c = 0; c < COLS; c++) 
+      total += matriz[r][c];
+  }
+
+  return total;
+}
+
 int main (void) {
   int M[ROWS][COLS], tamMatriz = ROWS * COLS;
   /* Captura dos valores para a Matriz */
@@ -60,6 +88,21 @@ int main (void) {
   int resSomaColunaDois = somaColuna(M, 1); 
 
   printf("\nValor da soma da linha 2 da matriz M: %d\n", resSomaColunaDois);
+
+  /* Soma da diagonal principal */
+  int resSomaDiagonalPrincipal = somaDiagonalPrincipal(M);
+
+  printf("\nValor da soma da diagonal principal da matriz M: %d\n", resSomaDiagonalPrincipal);
+
+  /* Soma da diagonal secundária*/
+  int resSomaDiagonalSecundaria = somaDiagonalSecundaria(M);
+
+  printf("\nValor da soma da diagonal secundaria da matriz M: %d\n", resSomaDiagonalSecundaria);
+
+  /* Soma de todos os elementos da matriz M */
+  int resSomaTotalMatriz = somaMatrizTotal(M);
+
+  printf("\nValor da soma de todos os elementos da matriz M: %d\n", resSomaTotalMatriz);
 
   return 0;
 }
