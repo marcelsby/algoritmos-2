35d190a0c30953c0346f3579279651686aa84a1f
Marcel Barbosa <marcel.sby.br@gmail.com>
2020-12-21 15:15:21 -0300

    Primeira versão incompleta do algoritmo que encontra repetições em uma matriz.

--- 1-lista-exercicios/exercicio26.c
+++ 1-lista-exercicios/exercicio26.c
@@ -4,36 +4,59 @@
 /* elemento aparece X vezes em A. */
 /* Marcel Seichi Barbosa Yamamoto - 2º TADS */
 #include <stdio.h>
+#include <string.h>
 
 int main (void) {
   int rows = 2;
   int columns = 2;
-  int tamMatriz = rows * columns;
   int A[rows][columns];
+  int tamMatriz = sizeof(A)/sizeof(int);
 
-  /* Atribuição dos valores às posições da matriz */
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      printf("\nDigite o valor para a posição A[%d][%d] da matriz: ", i + 1, j + 1);
-      scanf("%i", &A[i][j]);
-    }
-  }
+  A[0][0] = 2;
+  A[0][1] = 2;
+  A[1][0] = 2;
+  A[1][1] = 2;
 
-  /* Validação para checar se existem números repetidos na matriz */
+  /* /\* Atribuição dos valores às posições da matriz *\/ */
+  /* for (int i = 0; i < rows; i++) { */
+  /*   for (int j = 0; j < columns; j++) { */
+  /*     printf("\nDigite o valor para a posição A[%d][%d] da matriz: ", i + 1, j + 1); */
+  /*     scanf("%i", &A[i][j]); */
+  /*   } */
+  /* } */
+
+  /* Porte da matriz para um vetor já agrupando os valores repetidos */
   int res[tamMatriz];
-  int resIdx = 0;
+  int resQtd[tamMatriz];
+  memset(res, 0, sizeof(res));
+  memset(resQtd, 0, sizeof(resQtd));
 
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; i < columns; j++) {
-      res[resIdx] = A[i][j];
-      resIdx++;
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < columns; c++) {
+      for (int i = 0; i < tamMatriz; i++) {
+	if (A[r][c] == res[i]) {
+	  resQtd[i]++;
+	} else {
+	  res[i] = A[r][c];
+	  resQtd[i]++;
+	}
+      }
     }
   }
+
+  /* printf("O elemento %d se repete %d vezes na matriz A.\n", res[i], repAux); */
   
+
+  /* Printando o array */
+  printf("\n");
   for (int i = 0; i < tamMatriz; i++) {
-    /* printf(" %d ", res[i]); */
-    printf(" %d ", tamMatriz);
+    printf(" %d ", res[i]);
   }
+  printf("\n");
+  for (int i = 0; i < tamMatriz; i++) {
+    printf(" %d ", resQtd[i]);
+  }
+  printf("\n");
   
   /* Imprimindo a matriz de forma organizada */
   printf("\n Matriz:\n");
